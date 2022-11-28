7df4ef124af35093474eb1b363a83a9cba43e85a
mhesselbarth <mhk.hesselbarth@gmail.com>
2022-11-28 09:52:40 -0500

    Update README

--- R/arrR.seagrass-packages.R
+++ R/arrR.seagrass-packages.R
@@ -1,5 +1,8 @@
 #' @description
-#' ADD TEXT
+#' Seagrass sub-processes of the arrR simulation model.  The goal of arrR.seagrass 
+#' is to provide all sub-processes related to the grid-based simulation environment
+#' of the arrR simulation model. This includes all procceses related to seagrass growth 
+#' and the water column i.e., all  sub-processes that are simulated grid-based.
 #'
 #' @name arrR.seagrass
 #' @docType package
